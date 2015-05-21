72340288b79f30a9a579bf961a6364b2de5afd7e
vnysingh <vinay@fitnessforce.com>
2015-05-21 15:00:35 +0530

    test 8

--- server/server.js
+++ server/server.js
@@ -3,9 +3,9 @@ var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
 
-app.start = function() {
+app.start = function () {
   // start the web server
-  return app.listen(function() {
+  return app.listen(function () {
     app.emit('started');
     console.log('Web server listening at: %s', app.get('url'));
   });
@@ -13,11 +13,17 @@ app.start = function() {
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
+boot(app, __dirname, function (err) {
   if (err) throw err;
 
-  app.use('/express-status', function(req, res, next) {
-  	res.json({ running: true , now : new Date()});
+  app.use('/express-status', function (req, res, next) {
+    res.json({running: true, now: new Date()});
+  });
+
+  app.use('/unittestreport', function (req, res, next) {
+    console.log(req);
+    console.log(res);
+    console.log(next);
   });
 
   // start the server if `$ node server.js`
